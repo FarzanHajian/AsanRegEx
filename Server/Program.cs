0e6785086bd435cf6a0e2832810b318cd88a6f32
farzanhajian <fh791@yahoo.com>
2023-02-22 12:22:40 +0400

    Google Analytics, Package update

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,23 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
+if (app.Environment.IsDevelopment()) app.UseWebAssemblyDebugging();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapRazorPages();
-app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.Run();
